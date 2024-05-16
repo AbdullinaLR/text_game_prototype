5d4c2495039ab511bc431342213e4583cc49d75f
AbdullinaLR <owlwithcookie@gmail.com>
2024-05-16 03:30:43 +0300

    все работает слава ангелине

--- manage.py
+++ manage.py
@@ -1,18 +1,18 @@
-
-# НЕ НАДО ВООБЩЕ
-from app import app, db
-from flask_migrate import Migrate
-from flask.cli import with_appcontext
-import click
-
-migrate = Migrate(app, db)
-
-@click.command(name='create_db')
-@with_appcontext
-def create_db():
-    db.create_all()
-
-app.cli.add_command(create_db)
-
-if __name__ == '__main__':
-    app.run()
+#
+# # НЕ НАДО ВООБЩЕ
+# from app import app, db
+# from flask_migrate import Migrate
+# from flask.cli import with_appcontext
+# import click
+#
+# migrate = Migrate(app, db)
+#
+# @click.command(name='create_db')
+# @with_appcontext
+# def create_db():
+#     db.create_all()
+#
+# app.cli.add_command(create_db)
+#
+# if __name__ == '__main__':
+#     app.run()
