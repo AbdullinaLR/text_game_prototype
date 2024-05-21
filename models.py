efbcd50f6c6950849f5d91b7ba01a66704e6da2b
AbdullinaLR <owlwithcookie@gmail.com>
2024-05-21 22:35:04 +0300

    базы данных персов, кнопки по середке

--- models.py
+++ models.py
@@ -1,3 +1,28 @@
+# from flask_sqlalchemy import SQLAlchemy
+#
+# db = SQLAlchemy()
+#
+# class Character(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(80), nullable=False)
+#     strength = db.Column(db.Integer, nullable=False)
+#     agility = db.Column(db.Integer, nullable=False)
+#     endurance = db.Column(db.Integer, nullable=False)
+#     story_index = db.Column(db.Integer, default=0)
+#     # story = db.Column(db.Text, default='')
+#     enemy_health = db.Column(db.Integer, nullable=True)
+#
+#     # def to_dict(self):
+#     #     return {
+#     #         'id': self.id,
+#     #         'name': self.name,
+#     #         'strength': self.strength,
+#     #         'agility': self.agility,
+#     #         'endurance': self.endurance,
+#     #         'story_index': self.story_index,
+#     #         'story': self.story,
+#     #         'enemy_health': self.enemy_health
+#     #     }
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -8,7 +33,18 @@ class Character(db.Model):
     strength = db.Column(db.Integer, nullable=False)
     agility = db.Column(db.Integer, nullable=False)
     endurance = db.Column(db.Integer, nullable=False)
+    story_index = db.Column(db.Integer, default=0)
     story = db.Column(db.Text, default='')
+    enemy_health = db.Column(db.Integer, nullable=True)
 
-    def __repr__(self):
-        return f'<Character {self.name}>'
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'strength': self.strength,
+            'agility': self.agility,
+            'endurance': self.endurance,
+            'story_index': self.story_index,
+            'story': self.story,
+            'enemy_health': self.enemy_health
+        }
