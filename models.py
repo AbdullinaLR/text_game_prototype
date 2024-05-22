5ed7e1af1c7066a42859b3abb737995be2d79efe
AbdullinaLR <owlwithcookie@gmail.com>
2024-05-23 00:01:23 +0300

    удаление лишнего

--- models.py
+++ models.py
@@ -1,28 +1,3 @@
-# from flask_sqlalchemy import SQLAlchemy
-#
-# db = SQLAlchemy()
-#
-# class Character(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80), nullable=False)
-#     strength = db.Column(db.Integer, nullable=False)
-#     agility = db.Column(db.Integer, nullable=False)
-#     endurance = db.Column(db.Integer, nullable=False)
-#     story_index = db.Column(db.Integer, default=0)
-#     # story = db.Column(db.Text, default='')
-#     enemy_health = db.Column(db.Integer, nullable=True)
-#
-#     # def to_dict(self):
-#     #     return {
-#     #         'id': self.id,
-#     #         'name': self.name,
-#     #         'strength': self.strength,
-#     #         'agility': self.agility,
-#     #         'endurance': self.endurance,
-#     #         'story_index': self.story_index,
-#     #         'story': self.story,
-#     #         'enemy_health': self.enemy_health
-#     #     }
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
